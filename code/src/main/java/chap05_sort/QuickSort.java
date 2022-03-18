698f06611966513742ddbdb52fc3cf63e4579528
kho903 <gmldnr2222@naver.com>
2022-03-18 14:32:00 +0900

    feat: Quick Sort

---
+++ code/src/main/java/chap05_sort/QuickSort.java
@@ -0,0 +1,41 @@
+package chap05_sort;
+
+public class QuickSort implements ISort {
+
+	@Override
+	public void sort(int[] arr) {
+		quickSort(arr, 0, arr.length - 1);
+	}
+
+	private void quickSort(int[] arr, int low, int high) {
+		if (low >= high) {
+			return;
+		}
+
+		int pivot = low + ((high - low) / 2);
+		int pivotValue = arr[pivot];
+
+		int left = low;
+		int right = high;
+
+		while (left <= right) {
+			while (arr[left] < pivotValue) {
+				left++;
+			}
+
+			while (arr[right] > pivotValue) {
+				right--;
+			}
+			if (left <= right) {
+				int tmp = arr[right];
+				arr[right] = arr[left];
+				arr[left] = tmp;
+				left++;
+				right--;
+			}
+		}
+
+		quickSort(arr, low, right); // 왼쪽
+		quickSort(arr, left, high); // 오른쪽
+	}
+}

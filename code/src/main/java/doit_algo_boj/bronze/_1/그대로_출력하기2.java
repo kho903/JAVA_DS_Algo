e857cbe81bc6dbd81248e49db266f358bcad2378
kho903 <gmldnr2222@naver.com>
2022-04-27 18:24:17 +0900

    feat: boj / 브론즈1 / 그대로_출력하기2

---
+++ code/src/main/java/doit_algo_boj/bronze/_1/그대로_출력하기2.java
@@ -0,0 +1,12 @@
+package doit_algo_boj.bronze._1;
+
+import java.util.Scanner;
+
+public class 그대로_출력하기2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+	}
+}
